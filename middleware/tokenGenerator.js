69f53be7c1c07d8fd7deffbbcb225ed88fceca32
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-12-12 20:43:48 +0400

    ####

--- middleware/tokenGenerator.js
+++ middleware/tokenGenerator.js
@@ -3,13 +3,7 @@ const jwt = require("jsonwebtoken");
 // Token generation
 const generateAccessToken = (payload) => {
   const secretKey = process.env.JWT_SECRET; // Use your actual secret key
-  const tokenType = "Bearer";
-
-  // Include token type in the JWT
-  const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
-  const accessToken = `${tokenType} ${token}`;
-
-  return accessToken; // Adjust expiresIn and other parameters as needed
+  return jwt.sign(payload, secretKey, { expiresIn: "1h" }); // Adjust expiresIn as needed
 };
 
 module.exports = generateAccessToken;
