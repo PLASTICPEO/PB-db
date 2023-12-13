f4a7be28225614d84e91abc2e46eee6d334370de
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-12-13 14:22:33 +0400

    ###

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,6 +3,19 @@ const User = require("../../models/user/index");
 
 const generateAccessToken = require("../../middleware/tokenGenerator");
 
+// const authenticateToken = (req, res, next) => {
+//   const token =
+//     req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+//   if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//     if (err) return res.status(403).json({ error: "Forbidden" });
+//     req.user = user; // Attach the decoded user information to the request object
+//     next();
+//   });
+// };
+
 const signIn = async (req, response) => {
   try {
     // Find the user by username
--- middleware/tokenGenerator.js
+++ middleware/tokenGenerator.js
@@ -1,9 +1,22 @@
+// middleware/tokenGenerator.js
+
 const jwt = require("jsonwebtoken");
 
-// Token generation
-const generateAccessToken = (payload) => {
-  const secretKey = process.env.JWT_SECRET; // Use your actual secret key
-  return jwt.sign(payload, secretKey, { expiresIn: "1h" }); // Adjust expiresIn as needed
+const generateAccessToken = (user) => {
+  const secretKey = process.env.JWT_SECRET;
+  return jwt.sign({ userId: user._id, username: user.username }, secretKey, {
+    expiresIn: "3h",
+  });
 };
 
 module.exports = generateAccessToken;
+
+// const jwt = require("jsonwebtoken");
+
+// // Token generation
+// const generateAccessToken = (payload) => {
+//   const secretKey = process.env.JWT_SECRET; // Use your actual secret key
+//   return jwt.sign(payload, secretKey, { expiresIn: "3h" }); // Adjust expiresIn as needed
+// };
+
+// module.exports = generateAccessToken;
